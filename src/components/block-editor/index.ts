ffb49bf6bce9b0330183badbad9ef2ff14cde4b3
varun-r-boop <varunpanthom6555@gmail.com>
2025-06-29 19:29:20 +0530

    build error fix

--- src/components/block-editor/index.ts
+++ src/components/block-editor/index.ts
@@ -1,5 +1,5 @@
-export { default as Block } from './Block';
 export { default as BlockEditor } from './BlockEditor';
 export { default as BlockPreview } from './BlockPreview';
+export { default as FloatingActionBar } from './FloatingActionBar';
 export { default as ProjectBlockPreview } from './ProjectBlockPreview';
 export { default as StylePanel } from './StylePanel'; 
